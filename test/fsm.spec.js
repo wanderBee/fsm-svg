4ec17061a3fec31d28d460292923cd9325aa3d06
wangpf <wander2th@gmail.com>
2019-06-18 17:53:48 +0800

    test(fsm): test scale(),resetState()

--- test/fsm.spec.js
+++ test/fsm.spec.js
@@ -23,7 +23,8 @@ describe("Link", () => {
 					linkTo: 1
 				},
 				{
-					label: "Warning"
+					label: "Warning",
+					circleR: 20
 				},
 				{
 					label: "Problematic"
@@ -37,5 +38,26 @@ describe("Link", () => {
 
 	it("should exists", () => {
 		expect(fsm).toBeDefined();
+	});
+
+	it("test method scale(), resetState()", () => {
+		let animeIndex = 1; // label Warning
+		let ocr = fsm._states[animeIndex].circleR;
+
+		expect(ocr).toEqual(20);
+
+		// scale big
+		fsm.scale(animeIndex, 1.4);
+		expect(fsm._states[animeIndex].circleR).toEqual(ocr * 1.4);
+
+		// restore
+		fsm.scale(animeIndex, 1);
+		expect(fsm._states[animeIndex].circleR).toEqual(ocr);
+
+		// resetState
+		fsm.resetState(animeIndex, {
+			circleR: 22
+		});
+		expect(fsm._states[animeIndex].circleR).toEqual(22);
 	});
 });
